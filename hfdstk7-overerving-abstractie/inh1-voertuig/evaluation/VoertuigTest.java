637700d1107856d528ee96de9b1fefbbff8d3105
Luk De Reu <luk.dereu@dekimo.com>
2024-02-21 16:56:04 +0100

    Fix error in test

--- hfdstk7-overerving-abstractie/inh1-voertuig/evaluation/VoertuigTest.java
+++ hfdstk7-overerving-abstractie/inh1-voertuig/evaluation/VoertuigTest.java
@@ -5,26 +5,26 @@ public class VoertuigTest
 {
     @Test
     public void testConstructor() {
-        var voertuig = new Auto("Volvo", "Groen");
+        var voertuig = new Voertuig("Volvo", "Groen");
     }
 
     @Test
     public void testMerk() {
-        var voertuig = new Auto("Volvo", "Groen");
+        var voertuig = new Voertuig("Volvo", "Groen");
         Assert.assertEquals("Volvo", voertuig.getMerk());
         
     }
 
     @Test
     public void testKleur() {
-        var voertuig = new Auto("Volvo", "Groen");
+        var voertuig = new Voertuig("Volvo", "Groen");
         Assert.assertEquals("Groen", voertuig.getKleur());
     }
 
 
     @Test
     public void testPrintInfo() {
-        var voertuig = new Auto("Volvo", "Groen");
+        var voertuig = new Voertuig("Volvo", "Groen");
         voertuig.printInfo();
     }
 
